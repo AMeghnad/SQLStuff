3136c2c09c575773a29c7c7d00b3875e551c5945
AMeghnad <anshumanmeghnad@gmail.com>
2018-03-12 12:11:19 +1100

    Initial upload

---
+++ Assets/Login.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour
+{
+    private string userName, eMail, passWord;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator CreateAccount(string username, string email, string password)
+    {
+        string createUserURL = "http://localhost/loginsystem/insertuser.php";
+        WWWForm user = new WWWForm();
+        user.AddField("username_Post", username);
+        user.AddField("email_Post", username);
+        user.AddField("password_Post", password);
+        WWW www = new WWW(createUserURL, user);
+        yield return www;
+        Debug.Log(www.text);
+        #region Notifications
+        Debug.Log("Sending");
+        StartCoroutine(CreateAccount(userName, eMail, passWord));
+        #endregion
+    }
+}
